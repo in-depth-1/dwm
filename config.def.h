19bbb0797202d0066a9400b9e071ed77f7461628
Mr.Jor <62343478+in-depth-1@users.noreply.github.com>
2023-03-01 11:54:54 +0800

    配置了 rofi 程序启动器

--- config.def.h
+++ config.def.h
@@ -18,7 +18,7 @@ static const char col_gray3[]      = "#bbbbbb";
 static const char col_gray4[]      = "#eeeeee";
 static const char col_cyan[]       = "#005577";
 static const char *colors[][3]     = {
-  /*               fg         bg         border   */
+  /*                fg         bg         border  */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
     [SchemeSel]  = {col_gray4, col_cyan,  col_cyan },
 };
@@ -27,7 +27,17 @@ static const char *colors[][3]     = {
 static const char *autostartscript = "$DWM/autostart.sh";
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {
+    "", /* tag:1  key:1  desc: terminal1 */
+    "", /* tag:2  key:f  desc: firefox   */
+    "", /* tag:3  key:i  desc: idea      */
+    "", /* tag:4  key:g  desc: workspace */
+    "", /* tag:5  key:m  desc: qqmusic   */
+    "", /* tag:6  key:2  desc: terminal2 */
+    "", /* tag:7  key:3  desc: terminal3 */
+    "", /* tag:8  key:4  desc: terminal4 */
+    "", /* tag:9  key:5  desc: terminal5 */
+};
 
 static const Rule rules[] = {
   /* xprop(1):
@@ -46,67 +56,63 @@ static const int resizehints    = 1;    /* 1 means respect size hints in tiled r
 static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
-  /* symbol     arrange function */
-    {"[]=", tile   }, /* first entry is default */
-    {"><>", NULL   }, /* no layout function means floating behavior */
-    {"[M]", monocle},
+    {"﬿", tile   }, /* 默认工作分区 */
+    {"", monocle},
 };
-
-/* key definitions: super */
-#define MODKEY Mod4Mask
-#define TAGKEYS(KEY, TAG)                                                                                                                                                                              \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}}, {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                                                  \
-      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                                                                                                                                                     \
   {                                                                                                                                                                                                    \
     .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                                                                                                                                               \
   }
+/* key definitions: super */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY, TAG, cmd)                                                                                                                                                                         \
+  {MODKEY, KEY, view, {.ui = 1 << TAG, .v = cmd}}, {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},
 
 static const Key keys[] = {
-  /* modifier            key        function        argument          */
-    {MODKEY,             XK_t,      togglebar,      {0}                   }, /* super + t              | 显示/隐藏状态栏 */
+  /* modifier            key        function        argument */
+    {MODKEY,             XK_t,      togglebar,      {0}                                                  }, /* super + t              | 显示/隐藏状态栏 */
 
-    {MODKEY,             XK_Tab,    focusstack,     {.i = +1}             }, /* super + tab            | 本 tag 内切换聚焦窗口 */
-    {MODKEY | ShiftMask, XK_Tab,    focusstack,     {.i = -1}             }, /* super + shift + tab    | 本 tag 内切换聚焦窗口 */
-    {MODKEY,             XK_Up,     focusstack,     {.i = -1}             }, /* super + up             | 本 tag 内切换聚焦窗口 */
-    {MODKEY,             XK_Down,   focusstack,     {.i = +1}             }, /* super + down           | 本 tag 内切换聚焦窗口 */
+    {MODKEY,             XK_Tab,    focusstack,     {.i = +1}                                            }, /* super + tab            | 本 tag 内切换聚焦窗口 */
+    {MODKEY | ShiftMask, XK_Tab,    focusstack,     {.i = -1}                                            }, /* super + shift + tab    | 本 tag 内切换聚焦窗口 */
+    {MODKEY,             XK_Up,     focusstack,     {.i = -1}                                            }, /* super + up             | 本 tag 内切换聚焦窗口 */
+    {MODKEY,             XK_Down,   focusstack,     {.i = +1}                                            }, /* super + down           | 本 tag 内切换聚焦窗口 */
 
-    {MODKEY,             XK_i,      incnmaster,     {.i = +1}             },
-    {MODKEY,             XK_d,      incnmaster,     {.i = -1}             },
+    {MODKEY,             XK_i,      incnmaster,     {.i = +1}                                            },
+    {MODKEY,             XK_d,      incnmaster,     {.i = -1}                                            },
 
-    {MODKEY,             XK_comma,  setmfact,       {.f = -0.05}          }, /* super + ,              | 降低窗口宽度 */
-    {MODKEY,             XK_period, setmfact,       {.f = +0.05}          }, /* super + .              | 增加窗口宽度 */
+    {MODKEY,             XK_comma,  setmfact,       {.f = -0.05}                                         }, /* super + ,              | 降低窗口宽度 */
+    {MODKEY,             XK_period, setmfact,       {.f = +0.05}                                         }, /* super + .              | 增加窗口宽度 */
 
-    {MODKEY | ShiftMask, XK_Return, zoom,           {0}                   }, /* super + shift + return | 将当前聚焦窗口置为主窗口 */
-    {MODKEY,             XK_Tab,    view,           {0}                   },
-    {MODKEY,             XK_q,      killclient,     {0}                   }, /* super + q              | 关闭当前窗口 */
-    {MODKEY,             XK_t,      setlayout,      {.v = &layouts[0]}    },
-    {MODKEY,             XK_f,      setlayout,      {.v = &layouts[1]}    },
-    {MODKEY,             XK_m,      setlayout,      {.v = &layouts[2]}    },
-    {MODKEY,             XK_space,  setlayout,      {0}                   },
-    {MODKEY | ShiftMask, XK_space,  togglefloating, {0}                   },
-    {MODKEY,             XK_0,      view,           {.ui = ~0}            },
-    {MODKEY | ShiftMask, XK_0,      tag,            {.ui = ~0}            },
-    {MODKEY,             XK_b,      focusmon,       {.i = -1}             }, /* super + b              | 光标移动到另一个显示器 */
-    {MODKEY | ShiftMask, XK_b,      focusmon,       {.i = +1}             }, /* super + shift + b      | 聚焦窗口移动到另一个显示器 */
-    {MODKEY | ShiftMask, XK_comma,  tagmon,         {.i = -1}             },
-    {MODKEY | ShiftMask, XK_period, tagmon,         {.i = +1}             },
-    {MODKEY,             XK_F12,    quit,           {0}                   }, /* super + F12            | 退出 dwm */
+    {MODKEY | ShiftMask, XK_Return, zoom,           {0}                                                  }, /* super + shift + return | 将当前聚焦窗口置为主窗口 */
+    {MODKEY,             XK_Tab,    view,           {0}                                                  },
+    {MODKEY,             XK_q,      killclient,     {0}                                                  }, /* super + q              | 关闭当前窗口 */
+    {MODKEY,             XK_t,      setlayout,      {.v = &layouts[0]}                                   },
+    {MODKEY,             XK_g,      setlayout,      {.v = &layouts[1]}                                   }, /* super + g              | 切换 grid 布局 */
+    {MODKEY,             XK_space,  setlayout,      {0}                                                  },
+    {MODKEY | ShiftMask, XK_space,  togglefloating, {0}                                                  },
+    {MODKEY,             XK_0,      view,           {.ui = ~0}                                           },
+    {MODKEY | ShiftMask, XK_0,      tag,            {.ui = ~0}                                           },
+    {MODKEY,             XK_b,      focusmon,       {.i = -1}                                            }, /* super + b              | 光标移动到另一个显示器 */
+    {MODKEY | ShiftMask, XK_b,      focusmon,       {.i = +1}                                            }, /* super + shift + b      | 聚焦窗口移动到另一个显示器 */
+    {MODKEY | ShiftMask, XK_comma,  tagmon,         {.i = -1}                                            },
+    {MODKEY | ShiftMask, XK_period, tagmon,         {.i = +1}                                            },
+    {MODKEY,             XK_F12,    quit,           {0}                                                  }, /* super + F12            | 退出 dwm */
 
-    {MODKEY,             XK_Return, spawn,          SHCMD("alacritty")    }, /* super + return         | 打开终端 */
-    {NULL,               XK_F1,     spawn,          SHCMD("flameshot gui")}, /* F1                     | 截图 */
+    {MODKEY,             XK_Return, spawn,          SHCMD("alacritty")                                   }, /* super + return         | 打开终端 */
+    {NULL,               XK_F1,     spawn,          SHCMD("flameshot gui")                               }, /* F1                     | 截图 */
+    {Mod1Mask,           XK_space,  spawn,          SHCMD("~/Workspace/dotfiles/scripts/callRofi.sh run")}, /* alt + space | 打开 rofi */
 
-    TAGKEYS(XK_1, 0)  /* 切换到 tag 1 */
-    TAGKEYS(XK_2, 1)  /* 切换到 tag 2 */
-    TAGKEYS(XK_3, 2)  /* 切换到 tag 3 */
-    TAGKEYS(XK_4, 3)  /* 切换到 tag 4 */
-    TAGKEYS(XK_5, 4)  /* 切换到 tag 5 */
-    TAGKEYS(XK_6, 5)  /* 切换到 tag 6 */
-    TAGKEYS(XK_7, 6)  /* 切换到 tag 7 */
-    TAGKEYS(XK_8, 7)  /* 切换到 tag 8 */
-    TAGKEYS(XK_9, 8)  /* 切换到 tag 9 */
+    TAGKEYS(XK_1, 0, 0)  /* 切换到 tag 1 */
+    TAGKEYS(XK_2, 1, "firefox")  /* 切换到 tag 2 */
+    TAGKEYS(XK_3, 2, 0)  /* 切换到 tag 3 */
+    TAGKEYS(XK_4, 3, 0)  /* 切换到 tag 4 */
+    TAGKEYS(XK_5, 4, 0)  /* 切换到 tag 5 */
+    TAGKEYS(XK_6, 5, 0)  /* 切换到 tag 6 */
+    TAGKEYS(XK_7, 6, 0)  /* 切换到 tag 7 */
+    TAGKEYS(XK_8, 7, 0)  /* 切换到 tag 8 */
+    TAGKEYS(XK_9, 8, 0)  /* 切换到 tag 9 */
 };
 
 /* button definitions */
